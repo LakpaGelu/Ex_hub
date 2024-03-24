fc23d842b4a5ee65a1ffd81d0535cc03a943b843
lakpaGelu <np03cs4s220003@heraldcollege.edu.np>
2024-03-24 21:45:30 +0545

    fix:

--- expertise_hub/settings.py
+++ expertise_hub/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='zukerbergmeta@gmail.com'
-EMAIL_HOST_PASSWORD ='tytzvzujqbphxetn'
+EMAIL_HOST_USER ='expertisehub777@gmail.com'
+EMAIL_HOST_PASSWORD ='cjcmgmbxvqodhlhz'
